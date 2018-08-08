d2f846777d954f24637d2019be4de473804bf48a
Joe Zhou <joe@EASTMAN-EIGHTY-ONE.MIT.EDU>
2018-08-08 16:31:51 -0400

    Error bars fixed as officially derived by Jesse

--- plot_dat.py
+++ plot_dat.py
@@ -15,8 +15,6 @@ default_range = {"hardest_pT": (0, 2000), "mul_pre_SD": (0, 100), "hardest_eta":
 default_title = {"hardest_pT": "hardest jet $p_T$", "mul_pre_SD": "mul. of the hardest jet", "hardest_eta": "hardest jet $\eta$", "hardest_phi": "hardest jet $\phi$", "hardest_area": "hardest jet area"}
 default_axis_labels = {"hardest_pT": ("hardest jet $p_T$ $[GeV]$", "diff. cross-section $[\mu b/GeV]$"), "mul_pre_SD": ("multiplicity", "diff. cross-section $[\mu b]$"), "hardest_eta": ("hardest jet $\eta$", "diff. cross-section $[\mu b]$"), "hardest_phi": ("hardest jet $\phi$", "diff. cross-section $[\mu b]$"), "hardest_area": ("hardest jet area", "diff. cross-section $[\mu b]$")}
 
-prescale_factors = {"HLT_Jet30": 1, "HLT_Jet60": 1, "HLT_Jet80": 1, "HLT_Jet110": 1, "HLT_Jet150": 1, "HLT_Jet190": 1, "HLT_Jet240": 1, "HLT_Jet300": 1, "HLT_Jet370": 1}
-
 def load_effective_lumi(effective_lumi_file_dir, data_files):
     data_file_list = [data_file.replace(".dat", ".mod") for data_file in data_files]
     total_effective_lumi = {"HLT_Jet30": 0.0, "HLT_Jet60": 0.0, "HLT_Jet80": 0.0, "HLT_Jet110": 0.0, "HLT_Jet150": 0.0, "HLT_Jet190": 0.0, "HLT_Jet240": 0.0, "HLT_Jet300": 0.0, "HLT_Jet370": 0.0}
@@ -35,7 +33,6 @@ def load_effective_lumi(effective_lumi_file_dir, data_files):
 def read_dat_to_list(effective_lumi_dic_for_DAT_file, DAT_file, eta_range_check_2point4, hardest_pT_lower_bound):
     var_list = []
     scale_list = []
-    prescale_list = []
     
     var_index = None
     trigger_fired_index = None
@@ -69,9 +66,8 @@ def read_dat_to_list(effective_lumi_dic_for_DAT_file, DAT_file, eta_range_check_
         elif ((not eta_range_check_2point4) or -2.4<=float(row[hardest_eta_index])<=2.4) and hardest_pT_lower_bound <= float(row[hardest_pT_index]):
             var_list.append(float(row[var_index]))
             scale_list.append(scaling_factors[row[trigger_fired_index]])
-            prescale_list.append(prescale_factors[row[trigger_fired_index]])
     
-    return (var_list, scale_list, prescale_list)
+    return (var_list, scale_list)
 
 ##################################################################################################################################################################################
 
@@ -126,22 +122,20 @@ scaling_factors = load_effective_lumi("./effective_luminosity_by_trigger.csv", d
 
 hist_data = []
 bin_edges = []
-no_of_events_in_bins = []
+sum_squared_weights = []
 
 for i in range(no_of_bins):
     hist_data.append(0.0)
-    no_of_events_in_bins.append(0.0)
+    sum_squared_weights.append(0.0)
 
 for data_file in data_files:
     DAT_file = csv.reader(open(input_directory + data_file), delimiter=' ', skipinitialspace = 1)
 
-    (var_list, scale_list, prescale_list) = read_dat_to_list(scaling_factors, DAT_file, eta_range_check_2point4, hardest_pT_lower_bound)
+    (var_list, scale_list) = read_dat_to_list(scaling_factors, DAT_file, eta_range_check_2point4, hardest_pT_lower_bound)
     (current_hist_data, bin_edges) = np.histogram(var_list, bins=no_of_bins, range = var_range, weights = [x*no_of_bins/(var_range[1]-var_range[0]) for x in scale_list])
     hist_data = list(map(add, current_hist_data, hist_data))
-    (no_of_events_in_bins_for_current_MOD, whatever) = np.histogram(var_list, bins=no_of_bins, range = var_range, weights = prescale_list)
-    no_of_events_in_bins = list(map(add, no_of_events_in_bins, no_of_events_in_bins_for_current_MOD))
-
-print("there are " + str(int(sum(no_of_events_in_bins))) + " events ")
+    (current_sum_squared_weights, whatever) = np.histogram(var_list, bins=no_of_bins, range = var_range, weights = [(x*no_of_bins/(var_range[1]-var_range[0])) * (x*no_of_bins/(var_range[1]-var_range[0])) for x in scale_list])
+    sum_squared_weights = list(map(add, sum_squared_weights, current_sum_squared_weights))
 
 if eta_range_check_2point4:
     var_title += " with $|\eta|\leq2.4$"
@@ -150,14 +144,7 @@ if (hardest_pT_lower_bound):
 pl.figure(var_title)
 pl.title(var_title)
 if with_error_bar:
-    y_error = []
-    for (sigma, n) in zip(hist_data, no_of_events_in_bins):
-        if n <= 0.0:
-            y_error.append(0.0)
-        else:
-            y_error.append(sigma/math.sqrt(n))
-    
-    pl.errorbar((bin_edges[:-1] + bin_edges[1:])/2, hist_data, yerr=y_error, label=var_label,fmt = 'r.')
+    pl.errorbar((bin_edges[:-1] + bin_edges[1:])/2, hist_data, yerr=np.sqrt(sum_squared_weights), label=var_label,fmt = 'r.')
 else:
     pl.plot((bin_edges[:-1] + bin_edges[1:])/2, hist_data, label=var_label, color='r')
 pl.xlabel(var_x_label)
